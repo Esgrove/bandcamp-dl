857384294cbba4430de59079b7ec2173e931fa89
Esgrove <esgrove@outlook.com>
2024-08-05 13:05:21 +0300

    add recursive zip option

--- src/bin/unzip.rs
+++ src/bin/unzip.rs
@@ -1,8 +1,13 @@
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::sync::LazyLock;
 
+use anyhow::Result;
 use clap::Parser;
 use colored::Colorize;
+
+static ZIP_EXTENSION: LazyLock<Option<&std::ffi::OsStr>> =
+    LazyLock::new(|| Some(std::ffi::OsStr::new("zip")));
 
 #[derive(Parser)]
 #[command(author, about = "Extract all zip files concurrently", version)]
@@ -13,6 +18,10 @@ struct Args {
     /// Overwrite existing files
     #[arg(short, long)]
     force: bool,
+
+    /// Get zip files recursively
+    #[arg(short, long)]
+    recursive: bool,
 
     /// Verbose output
     #[arg(short, long)]
@@ -32,18 +41,7 @@ async fn main() -> anyhow::Result<()> {
         );
     }
 
-    let zip_files: Vec<PathBuf> = fs::read_dir(&input_path)?
-        .filter_map(|entry| {
-            let entry = entry.ok()?;
-            let path = entry.path();
-            if path.extension() == Some(std::ffi::OsStr::new("zip")) {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .collect();
-
+    let zip_files = gather_zip_files(&input_path, args.recursive)?;
     if zip_files.is_empty() {
         anyhow::bail!("No zip files found")
     }
@@ -54,6 +52,8 @@ async fn main() -> anyhow::Result<()> {
         println!("Extracting 1 zip file");
     };
 
+    // TODO: fix for recursive
+    // Move to extract and return number of files added in total
     let file_count_at_start = bandcamp_dl::utils::count_files_in_directory(&input_path)?;
 
     bandcamp_dl::extract_zip_files(zip_files, args.force).await;
@@ -66,4 +66,39 @@ async fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn gather_zip_files(input_path: &PathBuf, recursive: bool) -> Result<Vec<PathBuf>> {
+    let zip_files = if recursive {
+        gather_zip_files_recursive(input_path)?
+    } else {
+        fs::read_dir(input_path)?
+            .filter_map(|entry| {
+                let entry = entry.ok()?;
+                let path = entry.path();
+                if path.extension() == *ZIP_EXTENSION {
+                    Some(path)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    };
+    Ok(zip_files)
+}
+
+fn gather_zip_files_recursive(dir: &Path) -> Result<Vec<PathBuf>> {
+    let mut zip_files: Vec<PathBuf> = Vec::new();
+    if dir.is_dir() {
+        for entry in (fs::read_dir(dir)?).flatten() {
+            let path = entry.path();
+            if path.is_dir() {
+                zip_files.extend(gather_zip_files_recursive(&path)?);
+            } else if path.extension() == *ZIP_EXTENSION {
+                zip_files.push(path);
+            }
+        }
+    }
+
+    Ok(zip_files)
 }
